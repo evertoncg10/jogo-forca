d454e95c0392e5ad6292bab78dfde954bc49b77e
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2020-04-26 19:32:35 -0300

    Implementação da classe Dictionary.

--- jogoforca/src/main/java/br/com/everton/jogoforca/core/Word.java
+++ jogoforca/src/main/java/br/com/everton/jogoforca/core/Word.java
@@ -2,4 +2,9 @@ package br.com.everton.jogoforca.core;
 
 public class Word {
 
+	public Word(String word) {
+
+	}
+	
+
 }
---
+++ jogoforca/src/main/java/br/com/everton/jogoforca/game/GameException.java
@@ -0,0 +1,9 @@
+package br.com.everton.jogoforca.game;
+
+@SuppressWarnings("serial")
+public class GameException extends RuntimeException {
+
+	public GameException(String message) {
+		super(message);
+	}
+}

9bc0c61693fdcb71f0f64b60caec4339fdbdc058
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2020-04-26 20:40:12 -0300

    Refatoração para transformar classe Dictionary em um Singleton.

--- jogoforca/src/main/java/br/com/everton/jogoforca/core/Dictionary.java
+++ jogoforca/src/main/java/br/com/everton/jogoforca/core/Dictionary.java
@@ -8,28 +8,40 @@ import br.com.everton.jogoforca.game.GameException;
 import br.com.everton.jogoforca.utils.RandomUtils;
 
 public class Dictionary {
-	
+
 	private static final String FILE_NAME = "dicionario.txt";
 	private List<String> words = new ArrayList<>();
-	
-	
+	private static Dictionary instance;
+
+	private Dictionary() {
+		load();
+	}
+
+	public static Dictionary getInstance() {
+		if (instance == null) {
+			instance = new Dictionary();
+		}
+
+		return instance;
+	}
+
 	private void load() {
-		
-		try(Scanner scanner = new Scanner(getClass().getResourceAsStream("/" + FILE_NAME))){
-			
-			while(scanner.hasNextLine()){
+
+		try (Scanner scanner = new Scanner(getClass().getResourceAsStream("/" + FILE_NAME))) {
+
+			while (scanner.hasNextLine()) {
 				String word = scanner.nextLine().trim();
 				words.add(word);
 			}
-			
-			if(words.size() == 0) {
+
+			if (words.size() == 0) {
 				throw new GameException("A lista de palavras não pode ser vazia.");
 			}
 		}
 	}
-	
+
 	public Word nextWord() {
-		
+
 		int pos = RandomUtils.newRandomNumber(0, words.size());
 		return new Word(words.get(pos));
 	}

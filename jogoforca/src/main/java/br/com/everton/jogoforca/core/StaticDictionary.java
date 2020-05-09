1f53496c98f1ca85384caf276b07b0e53b7f90bb
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2020-05-09 17:18:02 -0300

    Criando múltiplos dicionários (Polimorfismo)

---
+++ jogoforca/src/main/java/br/com/everton/jogoforca/core/StaticDictionary.java
@@ -0,0 +1,21 @@
+package br.com.everton.jogoforca.core;
+
+import java.util.List;
+
+public class StaticDictionary extends Dictionary {
+
+	private List<String> words = List.of("Casa", "Computador", "Caneta", "Carro");
+	private int currentIndex = -1;
+
+	@Override
+	public Word nextWord() {
+		currentIndex = (currentIndex + 1) % words.size();
+		return new Word(words.get(currentIndex));
+	}
+
+	@Override
+	public String getName() {
+		return "Estático";
+	}
+
+}
